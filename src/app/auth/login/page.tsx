1ec97aae3d8b27a6e92edfd5435c634c1acd5b62
Rodrigo Osorio <rodrigo.osorio.rl@gmail.com>
2026-04-27 14:07:33 -0600

    cambio en el login, quitamos suspence

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,20 +1,11 @@
-import { Suspense } from 'react';
+/**
+ * @module LoginPage
+ * @hu HU001
+ * @description Punto de entrada para la autenticación SSO.
+ */
+
 import { LoginForm } from "@/src/features/auth/ui/LoginForm";
 
 export default function LoginPage() {
-  return (
-    // Esto "cura" el error de prerenderizado al aislar useSearchParams()
-    <Suspense fallback={<LoginSkeleton />}>
-      <LoginForm />
-    </Suspense>
-  );
-}
-
-function LoginSkeleton() {
-  return (
-    <div className="w-full animate-pulse">
-      <div className="h-10 bg-gray-200 rounded w-1/2 mb-4"></div>
-      <div className="h-52 bg-gray-100 rounded-xl"></div>
-    </div>
-  );
+  return <LoginForm />;
 }
